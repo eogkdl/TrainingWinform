fac15a81e19a7525d1cd9462fde5f11d8a4b8893
eogkdl <kanezzong@naver.com>
2020-06-15 12:30:55 +0900

    Generic 소스 수정

--- Generic/Generic/Program.cs
+++ Generic/Generic/Program.cs
@@ -6,11 +6,48 @@ using System.Threading.Tasks;
 
 namespace Generic
 {
+    public class SimpleGeneric<T>
+    {
+        private T[] values;
+        private int index;
+
+        public SimpleGeneric(int len)
+        {
+            values = new T[len];
+            index = 0;
+        }
+
+        public void Add(params T[] args)
+        {
+            foreach (T item in args)
+            {
+                values[index++] = item;
+            }
+        }
+        public void Print()
+        {
+            foreach (T item in values)
+            {
+                Console.WriteLine(item + ",");
+            }
+            Console.WriteLine();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ㅎㅇ");
+            SimpleGeneric<Int32> ginterger = new SimpleGeneric<int>(10);
+            SimpleGeneric<double> gDouble = new SimpleGeneric<double>(10);
+
+            ginterger.Add(1, 2);
+            ginterger.Add(1, 2, 3, 4, 5, 6, 7);
+            ginterger.Add(10);
+
+            gDouble.Add(10.0, 12.4, 37.5);
+            ginterger.Print();
+            gDouble.Print();
+
         }
     }
 }
